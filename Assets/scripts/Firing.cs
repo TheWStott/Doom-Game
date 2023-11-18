df938962cb79f6fb90b838b159b3446c2fedd241
TheWStott <129526350+TheWStott@users.noreply.github.com>
2023-11-18 12:29:10 +0000

    Update Firing.cs

--- Assets/scripts/Firing.cs
+++ Assets/scripts/Firing.cs
@@ -51,7 +51,7 @@ public class Firing : MonoBehaviour {
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {  
         //Debug.Log("HIT!"+other.otherCollider.tag);
         Debug.Log("HIT!" + other.gameObject.tag);
